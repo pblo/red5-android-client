dfd61aedb4194ba6943663a69a8978cf83415628
pblo <pblo28@gmail.com>
2015-07-22 11:11:56 +0200

    initialiaze project

---
+++ app/src/main/java/pl/sii/red5_android_client/MainActivity.java
@@ -0,0 +1,16 @@
+package pl.sii.red5_android_client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
